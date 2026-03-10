9e411abe33b57ba2205c8e55562ae0ba04172f2d
Solenad <roe.dizon80@gmail.com>
2026-03-10 23:14:48 +0800

    fix: missing brace

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -42,23 +42,23 @@ return {
     },
     keys = {
       -- Chat
-      { "<leader>cc", "<cmd>CopilotChatToggle<cr>",      desc = "[Chat] Toggle",          mode = { "n", "v" } },
-      { "<leader>cm", "<cmd>CopilotChatModels<cr>",      desc = "[Chat] Select Model" },
-      { "<leader>cr", "<cmd>CopilotChatReset<cr>",       desc = "[Chat] Reset" },
-      { "<leader>cs", "<cmd>CopilotChatStop<cr>",        desc = "[Chat] Stop Output" },
+      { "<leader>cc", "<cmd>CopilotChatToggle<cr>",        desc = "[Chat] Toggle",          mode = { "n", "v" } },
+      { "<leader>cm", "<cmd>CopilotChatModels<cr>",        desc = "[Chat] Select Model" },
+      { "<leader>cr", "<cmd>CopilotChatReset<cr>",         desc = "[Chat] Reset" },
+      { "<leader>cs", "<cmd>CopilotChatStop<cr>",          desc = "[Chat] Stop Output" },
       -- Debug
-      { "<leader>cd", "<cmd>CopilotChatDebug<cr>",       desc = "[Debug] Debug Code",     mode = "v" },
-      { "<leader>cf", "<cmd>CopilotChatFix<cr>",         desc = "[Debug] Fix Code",       mode = "v" },
+      { "<leader>cd", "<cmd>CopilotChatDebug<cr>",         desc = "[Debug] Debug Code",     mode = "v" },
+      { "<leader>cf", "<cmd>CopilotChatFix<cr>",           desc = "[Debug] Fix Code",       mode = "v" },
       { "<leader>cF", "<cmd>CopilotChatFixDiagnostic<cr>", desc = "[Debug] Fix Diagnostic", mode = "v" },
       -- Documentation
-      { "<leader>cD", "<cmd>CopilotChatDocs<cr>",        desc = "[Docs] Generate Docs",   mode = "v" },
+      { "<leader>cD", "<cmd>CopilotChatDocs<cr>",          desc = "[Docs] Generate Docs",   mode = "v" },
       -- Explain
-      { "<leader>ce", "<cmd>CopilotChatExplain<cr>",     desc = "[Explain] Explain Code", mode = "v" },
+      { "<leader>ce", "<cmd>CopilotChatExplain<cr>",       desc = "[Explain] Explain Code", mode = "v" },
       -- Refactor
-      { "<leader>cR", "<cmd>CopilotChatRefactor<cr>",    desc = "[Refactor] Refactor",    mode = "v" },
-      { "<leader>co", "<cmd>CopilotChatOptimize<cr>",    desc = "[Refactor] Optimize",    mode = "v" },
-      { "<leader>cv", "<cmd>CopilotChatReview<cr>",      desc = "[Refactor] Review",      mode = "v" },
+      { "<leader>cR", "<cmd>CopilotChatRefactor<cr>",      desc = "[Refactor] Refactor",    mode = "v" },
+      { "<leader>co", "<cmd>CopilotChatOptimize<cr>",      desc = "[Refactor] Optimize",    mode = "v" },
+      { "<leader>cv", "<cmd>CopilotChatReview<cr>",        desc = "[Refactor] Review",      mode = "v" },
       -- Tests
-      { "<leader>ct", "<cmd>CopilotChatTests<cr>",       desc = "[Tests] Generate Tests", mode = "v" },
-  },
-}
+      { "<leader>ct", "<cmd>CopilotChatTests<cr>",         desc = "[Tests] Generate Tests", mode = "v" },
+    },
+  } }
