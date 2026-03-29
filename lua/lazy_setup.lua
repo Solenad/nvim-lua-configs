34ac8efcb0ef0c78ec5c1fe90079b953fec7c190
Solenad <roe.dizon80@gmail.com>
2026-03-29 21:58:16 +0800

    feat: added markdownpreview.nvim

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -221,6 +221,17 @@ require("lazy").setup({
         },
       },
     },
+    -- install with yarn or npm
+    {
+      "iamcco/markdown-preview.nvim",
+      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+      build = "cd app && yarn install",
+      init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+      end,
+      ft = { "markdown" },
+    },
+
     config = function()
       ---@type opencode.Opts
       vim.g.opencode_opts = {
